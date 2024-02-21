2b30c86c46201e470748df42505da8e77af3bd54
grouville <guillaume@dagger.io>
2024-02-21 10:45:51 -0800

    chore: temporary workaround - fix codegen
    To fix codegen, rename the Postgres.Client() to Postgres.Cli()
    
    Signed-off-by: grouville <guillaume@dagger.io>

--- postgres/client.go
+++ postgres/client.go
@@ -7,7 +7,7 @@ import (
 
 // Client returns a configured client with user and password set.
 // Note: if you want to set the host, you can append this option on usage
-func (p *Postgres) Client() (*Container, error) {
+func (p *Postgres) Cli() (*Container, error) {
 	cliOpts := []string{"/bin/sh", "-c", "psql"}
 
 	ctr := dag.
